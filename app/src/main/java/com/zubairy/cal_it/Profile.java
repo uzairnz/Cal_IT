9897adcdf366279222f19c717ca0407da59032d0
uzairnz <uzairnz@github.com>
2019-07-29 04:11:18 -0700

    Delete User Added

--- app/src/main/java/com/zubairy/cal_it/Profile.java
+++ app/src/main/java/com/zubairy/cal_it/Profile.java
@@ -2,6 +2,7 @@ package com.zubairy.cal_it;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
@@ -9,8 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,20 +27,17 @@ public class Profile extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private TextView txtDetails;
     private EditText inputName, inputPhone , inputAge , inputWeight , inputHeight;
-    private Button btnSave;
+    private Button btnSave, delusr;
     private Button mBtnLogout;
     private DatabaseReference mFirebaseDatabase;
     private FirebaseDatabase mFirebaseInstance;
 
     String userId = FirebaseAuth.getInstance().getUid();
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
 
         inputName = (EditText) findViewById(R.id.edit1);
         inputPhone = (EditText) findViewById(R.id.edit3);
@@ -43,6 +45,7 @@ public class Profile extends AppCompatActivity {
         inputWeight = (EditText) findViewById(R.id.edit5);
         inputHeight = (EditText) findViewById(R.id.edit6);
         btnSave = (Button) findViewById(R.id.editbutton1);
+        delusr = (Button) findViewById(R.id.editbutton3);
         mBtnLogout = findViewById(R.id.editbutton2);
 
         mFirebaseInstance = FirebaseDatabase.getInstance();
@@ -82,6 +85,26 @@ public class Profile extends AppCompatActivity {
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
                   finish();
+            }
+        });
+        delusr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                user.delete()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Intent intent = new Intent(Profile.this,login.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
+                                    finish();
+                                    Toast.makeText(getApplicationContext(),"Account Deleted Successfully!",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
@@ -144,7 +167,9 @@ public class Profile extends AppCompatActivity {
                 Log.e(TAG, "Failed to read user", error.toException());
             }
         });
+
     }
+
 
     private void updateUser(String name,  String phone , String age , String height , String weight) {
         // updating the user via child nodes
